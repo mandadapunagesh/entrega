5b64336fd25a224fb5b7e7e185fd85ce5694e877
mandadapunagesh <mandadapunagesh@gmail.com>
2014-05-29 16:01:51 -0400

    updates

--- user_data_script.sh
+++ user_data_script.sh
@@ -2,21 +2,30 @@
 set -e -x
 
 apt-get --yes --quiet update
-apt-get --yes --quiet install git puppet-common apache2
+apt-get --yes --quiet install puppet-common
+apt-get upgrade -y
+apt-get install apache2 -y
 
 #
 # Fetch puppet configuration from public github repository.
 #
 
-mv /etc/puppet /etc/puppet.orig
-git clone https://github.com/mandadapunagesh/entrega.git /etc/puppet
+#mv /etc/puppet /etc/puppet.orig
+#git clone https://github.com/mandadapunagesh/entrega.git /etc/puppet
 
 #
 # Run puppet, build site.
 #
+#puppet apply /etc/puppet/manifests/init.pp
 
-puppet apply /etc/puppet/manifests/init.pp
 
+mkdir -p /var/www/candidateeval/public_html
+chown -R $USER:$USER /var/www/candidateeval/public_html
+chmod -R 755 /var/www
+echo "<html><body><h1>Welcome</h1>" > /var/www/index.html
+echo "I was generated from user-data and cloud-init" >> /var/www/index.html
+echo "</body></html>" >> /var/www/index.html
+cp /etc/apache2/sites-available/default /etc/apache2/sites-available/candidateeval
 #
 # Load candidateeval virtual site and restart service - keep it simple
 #
