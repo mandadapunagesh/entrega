45eab17955d630f8eb5fbe843932230fe147dbf6
mandadapunagesh <mandadapunagesh@gmail.com>
2014-05-29 15:20:21 -0400

    New Files added

---
+++ user_data_script.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e -x
+
+apt-get --yes --quiet update
+apt-get --yes --quiet install git puppet-common apache2
+
+#
+# Fetch puppet configuration from public github repository.
+#
+
+mv /etc/puppet /etc/puppet.orig
+git clone https://github.com/mandadapunagesh/entrega.git /etc/puppet
+
+#
+# Run puppet, build site.
+#
+
+puppet apply /etc/puppet/manifests/init.pp
+
+#
+# Load eval virtual site and restart service - keep it simple
+#
+a2ensite eval
+service apache2 reload
